82f8349fda919f7ab3f38a7cfdb213350e766a63
TurtleTekFTC <108955484+TurtleTekFTC@users.noreply.github.com>
2023-09-19 18:49:29 -0500

    Adding important stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveWithSticksMecanum.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="MecanumTest", group="Turtle Group")
+public class DriveWithSticksMecanum extends LinearOpMode {
+    RobotHardware_TT   robot       = new RobotHardware_TT(this);
+    @Override
+    public void runOpMode() {
+        robot.init();
+        waitForStart();
+        boolean ToggleSpeed = false;
+        boolean PreviousToggleReading = gamepad1.left_bumper;
+        while (opModeIsActive()) {
+
+            double Strafe = 0;
+            double Turn = 0;
+            double Drive = 0;
+
+
+            
+
+            if (-gamepad1.left_stick_x < 0) {
+                Strafe = -Math.pow(-gamepad1.left_stick_x, 2);
+            } else {
+                Strafe = Math.pow(-gamepad1.left_stick_x, 2);
+            }
+
+            if (-gamepad1.right_stick_x < 0) {
+                Turn = -Math.pow(-gamepad1.right_stick_x, 2);
+            } else {
+                Turn = Math.pow(-gamepad1.right_stick_x, 2);
+            }
+
+            if (gamepad1.left_stick_y < 0) {
+                Drive = -Math.pow(gamepad1.left_stick_y, 2);
+            } else {
+                Drive = Math.pow(gamepad1.left_stick_y, 2);
+            }
+
+
+
+            if (!PreviousToggleReading && gamepad1.left_bumper) {
+                ToggleSpeed = !ToggleSpeed;
+            }
+            PreviousToggleReading = gamepad1.left_bumper;
+
+
+
+
+            if (ToggleSpeed == true) {
+                robot.mecanumDrive(Strafe * 0.7, Turn * 0.6, Drive * 0.6);
+            }
+            else {
+                robot.mecanumDrive(Strafe * 0.4, Turn * 0.3, Drive * 0.3);
+            }
+        }
+    }
+}
