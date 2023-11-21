2c5638523c2cb5666cbc209cfd93e0335a36bef0
ChickenChaser20736 <schneider.m.garrett@gmail.com>
2023-11-21 17:09:40 -0600

    Got it fixed. I added the program we made to make the robot drive for inches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFrontAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFrontAuto.java
@@ -3,22 +3,33 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import java.util.List;
+
 @Autonomous(name="RedFrontAuto", group="Turtle Group")
 public class RedFrontAuto extends LinearOpMode {
     RobotHardware_TT   robot       = new RobotHardware_TT(this);
 
 
-    @Override
-    public void runOpMode() {
-        robot.initAuto();
-        waitForStart();
-        boolean objectFound = false;
-        int objectRecognized = -1;
-        //At this point, we have the recognition. Loretta, do your homework
-        if(objectFound);
-            if(objectRecognized == 0){
+    double tickToMMRatio = 0.561 / 1;
+    double distance = 0;
+    double desiredDistance = 25000;
+    int startEncoderValue;
+    //19.2 * 28 = 96πmm <-- Replace these numbers.
+    //537.6 ticks = 301.6mm
+    // 1 tick = 0.561mm
 
-            }
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotHardware_TT robotHardware = new RobotHardware_TT(this);
+        robotHardware.init();
+        robotHardware.getEncoders();
+        List<Integer> encoderList = robotHardware.getEncoders();
+        startEncoderValue = encoderList.get(0);
+        while (opModeIsActive() && distance < desiredDistance) {
+            robotHardware.mecanumDrive(0,1,0);
+            encoderList = robotHardware.getEncoders();
+            distance = (encoderList.get(0) - startEncoderValue) * tickToMMRatio;
+        }
     }
 
 
