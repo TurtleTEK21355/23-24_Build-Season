f9b5da6ce701ecfbe77b18763fa54d7cc20c3c6e
ChickenChaser20736 <schneider.m.garrett@gmail.com>
2024-01-11 16:19:20 -0600

    Made the teleop for the operator far more intuitive, changing a few controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_23_24.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_23_24.java
@@ -3,12 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="TeleOp", group="TeleOp")
+@TeleOp(name="Competition TeleOp", group="TeleOp")
 public class TeleOp_23_24 extends LinearOpMode {
     RobotHardware_TT   robot       = new RobotHardware_TT(this);
     @Override
     public void runOpMode() {
         robot.init();
+        robot.resetImu();
 
         waitForStart();
         boolean ToggleSpeed = false;
@@ -17,6 +18,8 @@ public class TeleOp_23_24 extends LinearOpMode {
         double Strafe = 0;
         double Drive = 0;
         double speed = 0.32;
+        double wristPosition = 0;
+
 
         while (opModeIsActive()) {
 
@@ -29,35 +32,32 @@ public class TeleOp_23_24 extends LinearOpMode {
             }
 
 
-            robot.setWrist(Math.abs(gamepad2.left_stick_y));
 
-
-
-
-
-//            if (gamepad2.right_bumper && gamepad2.left_bumper) {
-//            } else if (gamepad2.left_bumper) {
-//                robot.setWrist(0.25);
-//            } else if (gamepad2.right_bumper) {
-//                robot.setWrist(0);
-//            }
+            if (gamepad2.right_trigger != 0 && gamepad2.left_trigger != 0) {
+            } else if (gamepad2.right_trigger > 0 && wristPosition < 0.5){ // FILLER NUMBER STILL NEED THE ACTUAL NUMBERS)
+                wristPosition = wristPosition + 0.1;
+                robot.setWrist(wristPosition);
+            } else if (gamepad2.left_trigger > 0 && wristPosition > 0) { // FILLER NUMBER STILL NEED THE ACTUAL NUMBERS)
+                wristPosition = wristPosition - 0.1;
+                robot.setWrist(wristPosition);
+            }
 
             //need new numbers!
             if (gamepad2.dpad_down && gamepad2.dpad_up) {
-            } else if (gamepad2.dpad_up) {
+            } else if (gamepad2.right_bumper) {
                 robot.setClaw(0.44);
-            } else if (gamepad2.dpad_down) {
+            } else if (gamepad2.left_bumper) {
                 robot.setClaw(0.64);
             }
 
 
 
             if ((gamepad2.y && gamepad2.a) || (gamepad2.y && gamepad2.b) || (gamepad2.a && gamepad2.b)) {
-            } else if (gamepad2.a) {
-                robot.setIntake(0.64);
             } else if (gamepad2.b) {
-                robot.setIntake(-0.64);
+                robot.setIntake(0.64);
             } else if (gamepad2.y) {
+                robot.setIntake(-0.64);
+            } else if (gamepad2.a) {
                 robot.setIntake(0);
             }
 
